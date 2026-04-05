5dab0c919955b1010c93ba862c59fb5447e4de7d
yukimizu <yukidokemizu7189@gmail.com>
2026-04-05 18:21:19 +0900

    issue #7,8,9

--- email-discord-bot/src/index.js
+++ email-discord-bot/src/index.js
@@ -3,15 +3,16 @@ import PostalMime from 'postal-mime';
 //日付の整形
 function formatDate(dateString) {
 	const date = new Date(dateString);
+  const jstdate = new Date(date.getTime() + 9 * 60 * 60 * 1000);
 
 	if (isNaN(date.getTime())) return "不明な日付";
 
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const year = jstdate.getUTCFullYear();
+  const month = (jstdate.getUTCMonth() + 1).toString().padStart(2, '0');
+  const day = jstdate.getUTCDate().toString().padStart(2, '0');
+  const hours = jstdate.getUTCHours().toString().padStart(2, '0');
+  const minutes = jstdate.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = jstdate.getUTCSeconds().toString().padStart(2, '0');
   return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
 }
 
